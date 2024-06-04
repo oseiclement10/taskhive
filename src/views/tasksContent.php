7608fb0bc8c177847bf65e2c143580b4c08cf26f
clement osei asante <clementoseiasante.nss@gmail.com>
2024-06-04 23:10:31 +0000

    setup

--- src/views/tasksContent.php
+++ src/views/tasksContent.php
@@ -39,13 +39,27 @@ function getBgColor($prop)
         </a>
     </div>
 
+    <div class="mb-6 ">
+        <?php
+        if (isset($_GET["errors"])) {
+            $errorsArr = explode("_", $_GET["errors"]);
+            foreach ($errorsArr as $error) {
+                echo "<p  class='notif bg-red-100 text-red-500 py-1 px-2 my-2 rounded-md'> $error </p>";
+            }
+        } else if (isset($_GET["success"])) {
+            echo "<p  class='notif bg-emerald-100 py-2 px-4 my-2 rounded-md text-green-600 lg:w-1/2'>" . $_GET["success"] . "</p>";
+        }
+        ?>
+    </div>
+
+
     <section class="grid grid-cols-3 w-5/6 gap-6">
         <?php
         foreach ($userTasks as $task) {
             echo '
                 <div class="border-[1px]  border-slate-200 rounded-xl px-4 py-3 ">
                 <div class="flex space-x-1 items-end justify-end">
-                    <a href="./tasks/new?mode=edit&id='.$task["id"].'">
+                    <a href="./tasks/new?mode=edit&id=' . $task["id"] . '">
                         <svg class="w-5 h-5 text-slate-600 dark:text-white hover:text-green-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
                         </svg>
